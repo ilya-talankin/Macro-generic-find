3ac41f855f96f4d7992424549ce403849467e834
ilya-talankin <115388729+ilya-talankin@users.noreply.github.com>
2023-02-27 00:28:42 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
 #include "all_finds_overloads.h"
+#include "assert.h"
 
 int main()
 {
     int ai[] = {4,5,6};
     float af[] = {1.5,2.5,3.5};
     char string[] = "ABCDabcd1234!@#$";
-    unsigned long long aull[] = {101, 102, 103};
-    printf("%d\n",FIND(int)(ai, sizeof(ai), 6.9));
-    /*printf("%d\n",FIND(float)(af, sizeof(af), 3.5));
-    printf("%d\n",FIND(char)(string, sizeof(string), '!'));
-    printf("%d\n",FIND(uint64_t)(aull, sizeof(aull), 103.0));
-    */
+    unsigned long long aull[] = {101, 102, 103, 104};
+
+    assert(2 == FIND(int)(ai, sizeof(ai), 6));
+    assert(1 == FIND(float)(af, sizeof(af), 2.5));
+    assert(8 == FIND(char)(string, sizeof(af), '1'));
+    assert(3 == FIND(uint64_t)(aull, sizeof(aull), 104.0));
+
     return 0;
 }
